20de6cfbbcde61030b2b53b24bf720e6a0d230dc
Ivan <thanhkahp2002@gmail.com>
2024-07-26 18:13:41 +0300

    add features

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Features = () => {
   return (
-    <div style={{height: 450}}>Features</div>
+    <div className='features'>
+      <h1>Go Premium</h1>
+      <h2 className='mt-16'>Get access to features:</h2>
+      <ul className='features__list mt-40'>
+        <li>Unlimited Subtitles Translation</li>
+        <li>Slow Down Playback</li>
+        <li>Share And Copy Words</li>
+      </ul>
+    </div>
   )
 }
 
